675f92368929e22bfbcea1ab3a57ae15c7cb5cea
Pavel Kuchin <pavel.s.kuchin@gmail.com>
2013-06-04 15:04:40 +0300

    JUnit tests for GameDao and UserDao
    + tests for all GameDao and UserDao methods
    + Spring configuration for JUnit tests (test-context.xml)

---
+++ src/test/java/com/checkers/server/dao/GameDaoTest.java
@@ -0,0 +1,174 @@
+package com.checkers.server.dao;
+
+import com.checkers.server.beans.Game;
+import com.checkers.server.beans.User;
+import com.checkers.server.beans.proxy.GameProxy;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Tests for {@link GameDaoImpl}
+ *
+ * @author Pavel Kuchin
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:test-context.xml")
+public class GameDaoTest {
+    @Autowired
+    GameDao gameDao;
+
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    public void newGame(){
+        //Creating new game from Game bean
+        Game game = new Game();
+
+        game.setGauid(null);
+
+        game.setName("Game name");
+        game.setDescription("Game description");
+
+        game.setState("open");
+        game.setType("long");
+        game.setBoard("8x8");
+
+        game.setBlack(userDao.getUser(1L));
+        game.setWhite(userDao.getUser(2L));
+
+        game.setModified(new Date());
+        game.setCreated(new Date());
+        game.setLastLogin(new Date());
+
+        gameDao.newGame(game);
+
+        Assert.assertNotNull("Game's object doesn't persist.", game.getGauid());
+    }
+
+    @Test
+    public void newGameProxy(){
+        //Creating new game from GameProxy bean
+        GameProxy game = new GameProxy();
+
+        game.setGauid(null);
+
+        game.setName("Game name");
+        game.setDescription("Game description");
+
+        game.setState("open");
+        game.setType("long");
+        game.setBoard("8x8");
+
+        game.setBlack(1L);
+        game.setWhite(2L);
+
+        game.setModified(new Date());
+        game.setCreated(new Date());
+        game.setLastLogin(new Date());
+
+        Game justGame = gameDao.newGame(game);
+
+        Assert.assertNotNull("GameProxy's object doesn't persist.", justGame.getGauid());
+        Assert.assertNotNull("Game's object is null in GameProxy method.", justGame);
+        Assert.assertNotNull("Game's object doesn't persist in GameProxy method.", justGame.getGauid());
+    }
+
+    @Test
+    public void getGame(){
+        //Creating new game from GameProxy bean
+        GameProxy game = new GameProxy();
+
+        game.setGauid(null);
+
+        game.setName("Game name");
+        game.setDescription("Game description");
+
+        game.setState("open");
+        game.setType("long");
+        game.setBoard("8x8");
+
+        game.setBlack(1L);
+        game.setWhite(2L);
+
+        game.setModified(new Date());
+        game.setCreated(new Date());
+        game.setLastLogin(new Date());
+
+        Game justGame = gameDao.newGame(game);
+
+        Game obtainedGame = gameDao.getGame(justGame.getGauid());
+
+        Assert.assertNotNull("Obtained game is null", obtainedGame);
+        Assert.assertEquals("Obtained game gauid is incorrect", justGame.getGauid(), obtainedGame.getGauid());
+    }
+
+    @Test
+    public void getUserGames(){
+        //Creating some new user
+        User user = new User();
+
+        user.setUuid(null);
+
+        user.setLogin("Konstantin.Konstantinopolskiy");
+        user.setFirstName("Konstantin");
+        user.setLastName("Konstantinopolskiy");
+        user.setPassword("jgu*Gg(m574mLU");
+
+        user.setEmail("Konstantin.Konstantinopolskiy@gmail.com");
+
+        user.setEnabled(true);
+
+        user.setRole("USER_ROLE");
+
+        user.setCreated(new Date());
+        user.setModified(new Date());
+        user.setLastLogin(new Date());
+
+        userDao.newUser(user);
+
+        //Creating three games for this user in the loop
+        for(int i = 0; i < 3; i++){
+            GameProxy game = new GameProxy();
+
+            game.setGauid(null);
+
+            game.setName("Game name");
+            game.setDescription("Game description");
+
+            game.setState("open");
+            game.setType("long");
+            game.setBoard("8x8");
+
+            game.setBlack(1L);
+            game.setWhite(user.getUuid());
+
+            game.setModified(new Date());
+            game.setCreated(new Date());
+            game.setLastLogin(new Date());
+
+            gameDao.newGame(game);
+        }
+
+        List<Game> userGame = gameDao.getUserGames(user.getUuid());
+
+        Assert.assertNotNull("User games list are null", userGame);
+        Assert.assertEquals("Not all user games are returned (or more than you need).", 3, userGame.size());
+    }
+
+    @Test
+    public void getGames(){
+        List<Game> games = gameDao.getGames();
+
+        Assert.assertNotNull("Games list is null.", games);
+        Assert.assertEquals("Not all games are returned (or more than you need).", 7, games.size());
+    }
+}
---
+++ src/test/java/com/checkers/server/dao/UserDaoTest.java
@@ -0,0 +1,91 @@
+package com.checkers.server.dao;
+
+import com.checkers.server.beans.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Tests for {@link UserDaoImpl}
+ *
+ * @author Pavel Kuchin
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:test-context.xml")
+public class UserDaoTest {
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    public void newUser(){
+        User user = new User();
+
+        user.setUuid(null);
+
+        user.setLogin("Konstantin.Konstantinopolskiy");
+        user.setFirstName("Konstantin");
+        user.setLastName("Konstantinopolskiyancitipate");
+        user.setPassword("jgu*Gg(m574mLU");
+
+        user.setEmail("Konstantin.Konstantinopolskiy@gmail.com");
+
+        user.setEnabled(true);
+
+        user.setRole("USER_ROLE");
+
+        user.setCreated(new Date());
+        user.setModified(new Date());
+        user.setLastLogin(new Date());
+
+        userDao.newUser(user);
+
+        Assert.assertNotNull("User's object doesn't persist.", user.getUuid());
+    }
+
+    @Test
+    public void getUser(){
+        //User creation.
+        User user = new User();
+
+        user.setUuid(null);
+
+        user.setLogin("Konstantin.Konstantinopolskiy");
+        user.setFirstName("Konstantin");
+        user.setLastName("Konstantinopolskiy");
+        user.setPassword("jgu*Gg(m574mLU");
+
+        user.setEmail("Konstantin.Konstantinopolskiy@gmail.com");
+
+        user.setEnabled(true);
+
+        user.setRole("USER_ROLE");
+
+        user.setCreated(new Date());
+        user.setModified(new Date());
+        user.setLastLogin(new Date());
+
+        userDao.newUser(user);
+
+        User obtainedUser = userDao.getUser(user.getUuid());
+
+        Assert.assertNotNull("Obtained user doesn't exist.", obtainedUser);
+        Assert.assertNotNull("Obtained user uuid is null.", obtainedUser.getUuid());
+        Assert.assertEquals("Obtained user uuid is incorrect.", user.getUuid(), obtainedUser.getUuid());
+    }
+
+    public void getUsers(){
+        List<User> users = userDao.getUsers();
+
+        Assert.assertNotNull("Users list is null.", users);
+        Assert.assertEquals("Not all records are returned (or more than you need).", 4, users.size());
+    }
+
+
+}
