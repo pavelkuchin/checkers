ed1251ce8036e5dd18081fdc7eafe3298fe14bee
Pavel Kuchin <pavel.s.kuchin@gmail.com>
2013-06-07 10:56:08 +0300

    RESTful Unit Tests for game ans step controllers
    + Base tests for game and step controllers
    ! You can run it only when server has been started.

---
+++ src/test/java/com/checkers/server/controllers/GameControllerTest.java
@@ -0,0 +1,92 @@
+package com.checkers.server.controllers;
+
+import static com.eclipsesource.restfuse.Assert.assertOk;
+import static com.eclipsesource.restfuse.Assert.assertCreated;
+
+import com.eclipsesource.restfuse.*;
+import com.eclipsesource.restfuse.annotation.Authentication;
+import com.eclipsesource.restfuse.annotation.Context;
+import com.eclipsesource.restfuse.annotation.HttpTest;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for {@link GameController}
+ *
+ * @author Pavel Kuchin
+ */
+
+@RunWith( HttpJUnitRunner.class )
+public class GameControllerTest {
+
+    @Rule
+    public com.eclipsesource.restfuse.Destination description = getDestination();
+
+    @Context
+    private Response response;
+
+    private Destination getDestination() {
+        Destination destination = new Destination( this, "http://localhost:8080/checkers/game" );
+        destination.getRequestContext().addHeader( "Content-Type", "application/json" );
+        return destination;
+    }
+
+    @HttpTest(  method = Method.GET,
+                path = "/1",
+                authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void getGame(){
+        assertOk( response );
+    }
+
+    @HttpTest(  method = Method.GET,
+            path = "/",
+            authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void getGames(){
+        assertOk( response );
+    }
+
+    @HttpTest(  method = Method.POST,
+            path = "/",
+            content = "{\n" +
+                    "    \"gauid\": null,\n" +
+                    "    \"name\": \"Just for test\",\n" +
+                    "    \"description\": \"Testing game\",\n" +
+                    "    \"type\": \"long\",\n" +
+                    "    \"board\": \"10x10\",\n" +
+                    "    \"state\": \"open\",\n" +
+                    "    \"white\": 2,\n" +
+                    "    \"black\": 1,\n" +
+                    "    \"created\": 1369311377195,\n" +
+                    "    \"modified\": 1369311377195,\n" +
+                    "    \"lastLogin\": 1369311377195\n" +
+                    "}",
+            authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void newGame(){
+        assertCreated( response );
+    }
+
+    @HttpTest(  method = Method.GET,
+            path = "/1/step",
+            authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void getGameSteps(){
+        assertOk( response );
+    }
+
+    @HttpTest(  method = Method.GET,
+            path = "/1/laststep",
+            authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void getGameLastStep(){
+        assertOk( response );
+    }
+
+}
---
+++ src/test/java/com/checkers/server/controllers/StepControllerTest.java
@@ -0,0 +1,58 @@
+package com.checkers.server.controllers;
+
+import static com.eclipsesource.restfuse.Assert.assertOk;
+import static com.eclipsesource.restfuse.Assert.assertCreated;
+
+import com.eclipsesource.restfuse.*;
+import com.eclipsesource.restfuse.annotation.Authentication;
+import com.eclipsesource.restfuse.annotation.Context;
+import com.eclipsesource.restfuse.annotation.HttpTest;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * Tests for {@link StepController}
+ *
+ * @author Pavel Kuchin
+ */
+
+@RunWith( HttpJUnitRunner.class )
+public class StepControllerTest {
+
+    @Rule
+    public com.eclipsesource.restfuse.Destination description = getDestination();
+
+    @Context
+    private Response response;
+
+    private Destination getDestination() {
+        Destination destination = new Destination( this, "http://localhost:8080/checkers/step" );
+        destination.getRequestContext().addHeader( "Content-Type", "application/json" );
+        return destination;
+    }
+
+    @HttpTest(  method = Method.POST,
+            path = "/",
+            content = "{\n" +
+                    "  \"suid\"  : null,\n" +
+                    "  \"game\"  : 1,\n" +
+                    "  \"user\"  : 1,\n" +
+                    "  \"step\"  : \"1-3\",\n" +
+                    "  \"created\": 1369311377195\n" +
+                    "}",
+            authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void newGame(){
+        assertCreated( response );
+    }
+
+    @HttpTest(  method = Method.GET,
+                path = "/1",
+                authentications = {
+                    @Authentication( type = AuthenticationType.BASIC, user = "admin", password = "admin" ) }
+    )
+    public void getStep(){
+        assertOk( response );
+    }
+}
